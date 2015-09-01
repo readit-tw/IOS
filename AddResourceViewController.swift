8820fc4fc89dcb74199a5a954facc671ad7c55db
Rajashri <jadhavrajashri49@gmail.com>
2015-09-01 13:18:44 +0530

    Clear fields + Add Resource

--- AddResourceViewController.swift
+++ AddResourceViewController.swift
@@ -8,10 +8,81 @@
 
 import Foundation
 import UIKit
+import JavaScriptCore
+
+@objc protocol AddResourceProtocol : JSExport{
+    func onSave(jsonData : String)
+}
+
+@objc class AddResourceClass: NSObject,AddResourceProtocol{
+    
+    var addResViewController : AddResourceViewController!
+    
+    init(addResViewController : AddResourceViewController) {
+        self.addResViewController = addResViewController
+    }
+    func onSave(jsonData : String){
+        
+        var urlString = "http://readit.thoughtworks.com/resources"
+        var request = NSMutableURLRequest(URL:NSURL(string:urlString)!)
+        var session = NSURLSession.sharedSession();
+        request.HTTPMethod = "POST"
+        
+        if let json =  NSJSONSerialization.JSONObjectWithData(jsonData.dataUsingEncoding(NSUTF8StringEncoding)!, options: nil, error: nil) as? NSDictionary{
+            
+            var err : NSError?
+            request.HTTPBody = NSJSONSerialization.dataWithJSONObject(json, options: nil, error: &err)
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
+                
+                if data != nil {
+                    var json = NSJSONSerialization.JSONObjectWithData(data , options: NSJSONReadingOptions.MutableContainers, error:nil) as? NSDictionary
+                    if err != nil{
+                        println(err?.localizedDescription)
+                        return
+                    }
+                    if let httpResponse = response as? NSHTTPURLResponse{
+                        if httpResponse.statusCode != 201 {
+                            self.showAlert("Oops", message:String(httpResponse.statusCode))
+                            return
+                        }
+                    }
+                }
+                
+                if error != nil{
+                    self.showAlert("Oops", message:error.localizedDescription)
+                    return
+                }
+                
+            }
+        }
+        self.showAlert("Added Successfully", message:"")
+    
+        if addResViewController != nil {
+            addResViewController.clearAll()
+        }
+        
+        
+        
+    }
+    
+    
+    func showAlert(title : String, message : String)
+    {
+        var alert = UIAlertView(title:title, message:message, delegate: nil, cancelButtonTitle: "OK")
+        alert.show();
+    }
+    
+    
+}
 
 class AddResourceViewController :	UIViewController {
     
     @IBOutlet weak var webView: UIWebView!
+    var context = JSContext()
+    
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -33,66 +104,23 @@ class AddResourceViewController :	UIViewController {
     }
     
     @IBAction func addResource(sender: AnyObject) {
-      
-      println("Done clicked")
-      var responce  = self.webView.stringByEvaluatingJavaScriptFromString("getResource()")
-      
-      if let result = responce {
-        println(result)
-        if let json =  NSJSONSerialization.JSONObjectWithData(result.dataUsingEncoding(NSUTF8StringEncoding)!, options: nil, error: nil) as? NSDictionary {
-                post(json)
-            }
-        }
-       
-    
-       self.webView.stringByEvaluatingJavaScriptFromString("clearAll()")
-        
+        var responce  = self.webView.stringByEvaluatingJavaScriptFromString("addView.addResource()")
+
     }
     
-
-    func post(jsonData : NSDictionary) {
-        
-        var urlString = "http://readit.thoughtworks.com/resources"
-        var request = NSMutableURLRequest(URL:NSURL(string:urlString)!)
-        var session = NSURLSession.sharedSession();
-        request.HTTPMethod = "POST"
-        
-        
-        var err : NSError?
-        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(jsonData, options: nil, error: &err)
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
-            
-            if data != nil {
-                var json = NSJSONSerialization.JSONObjectWithData(data , options: NSJSONReadingOptions.MutableContainers, error:nil) as? NSDictionary
-                if err != nil{
-                    println(err?.localizedDescription)
-                    return
-                }
-                if let httpResponse = response as? NSHTTPURLResponse{
-                    if httpResponse.statusCode != 201 {
-                        self.showAlert("Oops", message:String(httpResponse.statusCode))
-                        return
-                    }
-                }
-            }
-            
-            if error != nil{
-                self.showAlert("Oops", message:error.localizedDescription)
-                return
-            }
-            
-        }
-        
+    func clearAll(){
+        self.webView.stringByEvaluatingJavaScriptFromString("addView.clearAll()")
     }
     
+}
+
+
+extension AddResourceViewController:UIWebViewDelegate {
     
-    func showAlert(title : String, message : String)
-    {
-        var alert = UIAlertView(title:title, message:message, delegate: nil, cancelButtonTitle: "OK")
-        alert.show();
+    func webViewDidFinishLoad(webView: UIWebView){
+        
+        context  =  webView.valueForKeyPath("documentView.webView.mainFrame.javaScriptContext") as! JSContext;
+        context.setObject(AddResourceClass(addResViewController: self) , forKeyedSubscript: "AddResource" )
+        
     }
-
-
 }
